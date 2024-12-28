d984204f9c2f0d2a5ebd303e2e59cc42f783f516
user <andrey.d.salomatin@gmail.com>
2024-12-28 16:39:13 +0300

    2 start and settings windows commit add: settings window(no escape) start window

--- main.py
+++ main.py
@@ -8,25 +8,27 @@ if __name__ == '__main__':
     pygame.display.set_caption('StepWar')
 
     size = 1400, 800
-    screen1 = mapping.Screen(size)
-    board = screen1.board
+    main_screen = mapping.Screen(size)
+    board = main_screen.board
+    button_start_game = main_screen.button_start_game
 
-    button_start_game = screen1.button_start_game
+    start_screen = start_window.Start_window(main_screen.sc, size)
 
     running = True
     fps = 120
     clock = pygame.time.Clock()
     while running:
+        start_screen.start()
         if button_start_game.gameplay:
-            start_game.start(screen1)
+            start_game.start(main_screen)
         else:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    screen1.get_click(event.pos, event.button)
-        screen1.sc.fill((0, 0, 0))
-        screen1.render(board, button_start_game)
+                    main_screen.get_click(event.pos, event.button)
+        main_screen.sc.fill((0, 0, 0))
+        main_screen.render(board, button_start_game)
         clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
--- settings.py
+++ settings.py
@@ -1,60 +1,70 @@
+import sys
+
 import pygame
 import mapping
-
-def check_click(mouse_pos, lst):
-    global running_settings
-    for button in lst:
-        if button.button_rect.left <= mouse_pos[0] <= button.button_rect.right and button.button_rect.top <= mouse_pos[
-            1] <= button.button_rect.bottom:
-            if button == lst[7]:
-                running_settings = False
+import start_window
 
 
-def render(screen, lst):
-    lst[0].render(screen)
-    lst[1].render(screen)
-    lst[2].render(screen)
-    lst[3].render(screen)
-    lst[4].render(screen)
-    lst[5].render(screen)
-    lst[6].render(screen)
-    lst[7].render(screen)
-    pygame.draw.rect(screen, pygame.Color('green'), pygame.Rect(180, 160, 970, 100), 1)
+class Settings_window:
+    def __init__(self, screen, size):
+        self.running = True
+        self.size = width, height = size
+        self.main_screen = screen
+        self.screen = pygame.surface.Surface((width, height))
 
-def open():
-    pygame.display.set_caption('Settings')
+        self.volume_button = mapping.Button('  Громкость  ', 80, 500, height // 2 - 320, 500, height // 2 - 320)
+        self.off_volume_button = mapping.Button('  Turn off  ', 50, 200, height // 2 - 200, 200, height // 2 - 200)
+        self.twenty_volume_button = mapping.Button('  20%  ', 50, 400, height // 2 - 200, 400, height // 2 - 200)
+        self.fourty_volume_button = mapping.Button('  40%  ', 50, 550, height // 2 - 200, 550, height // 2 - 200)
+        self.sixty_volume_button = mapping.Button('  60%  ', 50, 700, height // 2 - 200, 700, height // 2 - 200)
+        self.eighty_volume_button = mapping.Button('  80%  ', 50, 850, height // 2 - 200, 850, height // 2 - 200)
+        self.hundred_volume_button = mapping.Button('  100%  ', 50, 1000, height // 2 - 200, 1000, height // 2 - 200)
+        self.back_button = mapping.Button('      Назад      ', 80, 500, height // 2, 500, height // 2)
 
-    size = width, height = 1400, 800
-    screen = pygame.display.set_mode(size)
+        self.lst_buttons = [self.volume_button, self.off_volume_button, self.twenty_volume_button,
+                            self.fourty_volume_button, self.sixty_volume_button, self.eighty_volume_button,
+                            self.hundred_volume_button, self.back_button]
 
-    volume_button = mapping.Button('  Громкость  ', 80, 500, height // 2 - 320, 500, height // 2 - 320)
-    off_volume_button = mapping.Button('  Turn off  ', 50, 200, height // 2 - 200, 200, height // 2 - 200)
-    twenty_volume_button = mapping.Button('  20%  ', 50, 400, height // 2 - 200, 400, height // 2 - 200)
-    fourty_volume_button = mapping.Button('  40%  ', 50, 550, height // 2 - 200, 550, height // 2 - 200)
-    sixty_volume_button = mapping.Button('  60%  ', 50, 700, height // 2 - 200, 700, height // 2 - 200)
-    eighty_volume_button = mapping.Button('  80%  ', 50, 850, height // 2 - 200, 850, height // 2 - 200)
-    hundred_volume_button = mapping.Button('  100%  ', 50, 1000, height // 2 - 200, 1000, height // 2 - 200)
-    back_button = mapping.Button('      Назад      ', 80, 500, height // 2, 500, height // 2)
-    lst_buttons = [volume_button, off_volume_button, twenty_volume_button, fourty_volume_button, sixty_volume_button,
-                   eighty_volume_button, hundred_volume_button, back_button]
-    running_settings = True
-    fps = 120
-    clock = pygame.time.Clock()
-    while running_settings:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running_settings = False
-                pygame.quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                check_click(event.pos, lst_buttons)
-        screen.fill((0, 0, 0))
-        render(screen, lst_buttons)
-        clock.tick(fps)
-        pygame.display.flip()
+    def check_click(self, mouse_pos, lst):
+        for button in lst:
+            if (button.button_rect.left <= mouse_pos[0] <= button.button_rect.right and
+                    button.button_rect.top <= mouse_pos[1] <= button.button_rect.bottom):
+                if self.volume_button:
+                    pass
+                elif self.off_volume_button:
+                    pass
+                elif self.twenty_volume_button:
+                    pass
+                elif self.fourty_volume_button:
+                    pass
+                elif self.sixty_volume_button:
+                    pass
+                elif self.eighty_volume_button:
+                    pass
+                elif self.hundred_volume_button:
+                    pass
+                elif button == self.back_button:
+                    self.running = False
 
+    def render(self):
+        for button in self.lst_buttons:
+            self.screen.blit(button.button_surface, (button.button_rect.x, button.button_rect.y))
+            self.screen.blit(button.text, button.button_rect)
 
+            button.check_collidepoint(button.rect_width, button.rect_height)
+        self.main_screen.blit(self.screen, (0, 0))
 
+    def start(self):
+        fps = 120
+        clock = pygame.time.Clock()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.check_click(event.pos, self.lst_buttons)
+            self.screen.fill((0, 0, 0))
+            self.render()
 
-
-
-
+            clock.tick(fps)
+            pygame.display.flip()
