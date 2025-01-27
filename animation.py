58a685eec1b393a669546f7ea8298949f36d2ea4
user <andrey.d.salomatin@gmail.com>
2025-01-27 16:23:49 +0300

    1 animation commit
    Added:
    initial animation support

--- animation.py
+++ animation.py
@@ -11,6 +11,26 @@ class AnimationParams:
         self.fps_mod = fps_mod
 
 
+class AnimationStep:
+    def __init__(self, animation, action, action_args, mirror):
+        self.animation = animation
+        self.action = action
+        self.action_args = action_args
+        self.mirror = mirror
+
+
+class AnimationChain:
+    def __init__(self):
+        self.steps = []
+
+    def add_step(self, animation, action=None, action_args=None, mirror=False):
+        self.steps.append(AnimationStep(animation, action, action_args, mirror))
+
+    def __iter__(self):
+        for i in reversed(self.steps):
+            yield i
+
+
 class AnimatedSprite(pygame.sprite.Sprite):
     def __init__(self, animations, x, y, group, scale_to, default_animation):
         super().__init__(group)
@@ -24,11 +44,13 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.current_animation = default_animation
         self.frame_idx = 0
         self.image = self.frames[self.current_animation][0]
+        self.mirror_current_animation = False
         self.callback = None
         self.gc = 0
 
     def cut_sheet(self, sheet, columns, rows, w, h, ltop_x, ltop_y):
         frames = []
+        mirror_frames = []
         for j in range(rows):
             for i in range(columns):
                 frame_location = (ltop_x + w * i, ltop_y + h * j)
@@ -38,15 +60,71 @@ class AnimatedSprite(pygame.sprite.Sprite):
                 frames.append(image)
         return frames
 
-    def next(self):
+    def next_frame(self):
         if self.gc % self.animations[self.current_animation].fps_mod == 0:
             self.frame_idx += 1
             if self.frame_idx == len(self.frames[self.current_animation]) and self.callback is not None:
-                self.callback(self)
+                self.callback()
             self.frame_idx %= len(self.frames[self.current_animation])
             self.image = self.frames[self.current_animation][self.frame_idx]
+            if self.mirror_current_animation:
+                self.image = pygame.transform.flip(self.image, True, False)
+
+    def gc_tick(self):
         self.gc += 1
 
-    def set_animation(self, animation, callback=None):
+    def set_animation(self, animation, callback=None, mirror_current_animation=False):
         self.current_animation = animation
+        self.mirror_current_animation = mirror_current_animation
+        self.frame_idx = 0
         self.callback = callback
+        self.gc = 0
+
+    def start_animation_chain(self, animation_chain):
+        def generate_callback(unit, animation, callback, action=None, action_args=None, mirror_animation=False):
+            def f():
+                unit.set_animation(animation, callback, mirror_animation)
+                if action is not None and action_args is not None:
+                    action(*action_args)
+
+            return f
+
+        last = None
+        for i in animation_chain:
+            last = generate_callback(self, i.animation, last, action=i.action, action_args=i.action_args,
+                                     mirror_animation=i.mirror)
+        last()
+
+
+class MovableAnimatedSprite(AnimatedSprite):
+    def __init__(self, animations, x, y, group, scale_to, default_animation):
+        super().__init__(animations, x, y, group, scale_to, default_animation)
+        self.derired_position = None
+        self.dx = 0
+        self.dy = 0
+        self.ax = self.rect.x
+        self.ay = self.rect.y
+        self.ticks_count = 0
+        self.current_tick = 0
+
+    def move(self, x, y):
+        fps_mod = self.animations[self.current_animation].fps_mod
+        frames_cnt = len(self.frames[self.current_animation])
+        self.ticks_count = fps_mod * frames_cnt
+        self.dx = x / self.ticks_count
+        self.dy = y / self.ticks_count
+        self.ax = self.rect.x
+        self.ay = self.rect.y
+        self.current_tick = 0
+
+    def move_tick(self):
+        if self.current_tick < self.ticks_count:
+            self.ax += self.dx
+            self.ay += self.dy
+            self.rect.x = self.ax
+            self.rect.y = self.ay
+            self.current_tick += 1
+
+    def set_position(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
--- bone_dragon.py
+++ bone_dragon.py
@@ -1,20 +1,17 @@
 import pygame
 
 import start_game
-from animation import AnimatedSprite, AnimationParams
+from animation import MovableAnimatedSprite, AnimationParams, AnimationChain
 
-# def start_move_callback
-
-
-class BoneDragon(AnimatedSprite):
-    def __init__(self, x, y, image_size, group):
-        sheet = pygame.image.load('images/team_images/bone_dragon3.png')
+class BoneDragon(MovableAnimatedSprite):
+    def __init__(self, x, y, image_size, group, ):
+        sheet = pygame.image.load('images/team_images/bone_dragon.png')
         animations = {
-            "idle": AnimationParams(sheet, 4, 1, 125, 125, 0, 0, 30),
+            "idle": AnimationParams(sheet, 4, 1, 125, 125, 0, 0, 40),
             "attack": AnimationParams(sheet, 7, 1, 170, 170, 0, 170, 10),
-            "start_move": AnimationParams(sheet, 3, 1, 170, 170, 0, 340, 10),
-            "move": AnimationParams(sheet, 6, 1, 170, 170, 0, 510, 10),
-            "stop_move": AnimationParams(sheet, 6, 1, 170, 170, 0, 680, 10)
+            "start_move": AnimationParams(sheet, 3, 1, 170, 170, 0, 340, 15),
+            "move": AnimationParams(sheet, 6, 1, 170, 170, 0, 510, 15),
+            "stop_move": AnimationParams(sheet, 6, 1, 170, 170, 0, 680, 20)
         }
         super().__init__(animations, x, y, group, image_size, "idle")
         self.step = 4
@@ -27,34 +24,49 @@ class BoneDragon(AnimatedSprite):
         self.name = 'dragon'
         self.title = 'Дракон'
 
-
     def update(self, *args, **kwargs):
-        self.next()
-
+        self.next_frame()
+        self.move_tick()
+        self.gc_tick()
 
     def choose_step(self, cell_coords, choose_cell, screen, is_attack):
-        self.x_now, self.y_now = cell_coords
-        self.select_y, self.select_x = choose_cell
-        # self.screen = screen
+        x_now, y_now = cell_coords
+        select_y, select_x = choose_cell
         self.is_attack = is_attack
         self.damage_plus = 0
 
-        if screen.board.field[self.select_y][self.select_x] == 2:
+        if screen.board.field[select_y][select_x] == 2:
             self.damage_plus = 15
 
         if not self.is_attack:
-            screen.board.board[self.y_now][self.x_now] = 0
-            screen.board.board[self.select_y][self.select_x] = 1
+            screen.board.board[y_now][x_now] = 0
+            screen.board.board[select_y][select_x] = 1
 
-            x = (self.select_x - self.x_now) * screen.board.one_size
-            y = (self.select_y - self.y_now) * screen.board.one_size
+            x = (select_x - x_now) * screen.board.cell_size
+            y = (select_y - y_now) * screen.board.cell_size
+            px = self.rect.x + x
+            py = self.rect.y + y
 
-            self.rect = self.rect.move(x, y)
+            mirror = x < 0
+            animation_chain = AnimationChain()
+            animation_chain.add_step("start_move", mirror=mirror)
+            steps = max([abs(select_x - x_now), abs(select_y - y_now)])
+            tx = x / steps
+            ty = y / steps
+            for i in range(1, steps + 1):
+                animation_chain.add_step("move", self.move, [tx, ty], mirror=mirror)
+            animation_chain.add_step("stop_move", self.set_position, [px, py], mirror=mirror)
+            animation_chain.add_step("idle")
+
+            self.start_animation_chain(animation_chain)
         else:
             start_game.give_damage(screen, (
-                self.select_x * screen.board.one_size + screen.board.left,
-                self.select_y * screen.board.one_size + screen.board.top), (self.select_x, self.select_y),
+                select_x * screen.board.cell_size + screen.board.left,
+                select_y * screen.board.cell_size + screen.board.top), (select_x, select_y),
                                    self.damage + self.damage_plus)
+
+
+
 
 
 def set_view_stock(screen, coords, size):
